590bf37e7c7dcacd048ab3ecd14ef90b77963d2c
Piotr Karpala <pkarpala@infusion.com>
2015-08-15 11:17:46 +0200

    init commit

---
+++ SecureDoors/Solver.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kattis.IO;
+
+namespace SecureDoors
+{
+    class Solver
+    {
+        private HashSet<string> accessLog = new HashSet<string>();
+
+        public void Solve(Stream inStream, Stream outStream)
+        {
+            Scanner scanner = new Scanner(inStream);
+
+            StreamWriter writer = new StreamWriter(outStream);
+
+            // read log size
+            var logEntriesCount = scanner.NextInt();
+            string name, result;
+
+            for (int i = 0; i < logEntriesCount; i++)
+            {
+                var operation = scanner.Next();
+                name = scanner.Next();
+
+                if (operation == "entry")
+                {
+                    if (!accessLog.Contains(name))
+                    {
+                        // first entry
+                        accessLog.Add(name);
+                        result = Entry(false, name);
+                    }
+                    else
+                    {
+                        // already in the building
+                        result = Entry(true, name);
+                    }
+                }
+                else
+                // exit
+                {
+                    if (accessLog.Contains(name))
+                    {
+                        accessLog.Remove(name);
+                        result = Exit(false, name);
+                    }
+                    else
+                    {
+                        // not in the building but exiting
+                        result = Exit(true, name);
+                    }
+                }
+
+                writer.WriteLine(result);
+            }
+
+
+            writer.Flush();
+        }
+
+        public string Entry(bool isAnomaly, string name)
+        {
+            return name + " entered" + (isAnomaly ? " (ANOMALY)" : string.Empty);
+        }
+
+        public string Exit(bool isAnomaly, string name)
+        {
+            return name + " exited" + (isAnomaly ? " (ANOMALY)" : string.Empty);
+        }
+    }
+}
---
+++ SecureDoors/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Text;
+using NUnit.Framework;
+
+namespace SecureDoors
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Solver_Should_GiveCorrectAnswer()
+        {
+            // Arrange
+            Solver solver = new Solver();
+            const string input =
+ @"8
+entry Abbey
+entry Abbey
+exit Abbey
+entry Tyrone
+exit Mason
+entry Demetra
+exit Latonya
+entry Idella";
+            const string expected = @"Abbey entered
+Abbey entered (ANOMALY)
+Abbey exited
+Tyrone entered
+Mason exited (ANOMALY)
+Demetra entered
+Latonya exited (ANOMALY)
+Idella entered
+";
+
+            // Act
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(input)))
+            using (var outMs = new MemoryStream())
+            {
+                solver.Solve(ms, outMs);
+                outMs.Position = 0;
+                var result = new StreamReader(outMs).ReadToEnd();
+
+                // Assert
+                Assert.That(result, Is.EqualTo(expected));
+            }
+        }
+    }
+}
